423d4d61ea829ab4e19354ea4a1df3bf1d62a77a
mustafaiam <mustafaiam.github@gmail.com>
2022-04-08 16:26:58 +0300

    Installed NuGet packages and implemented SuperHero class

---
+++ EFCoreLoadRelatedData/EFCoreLoadRelatedData/SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCoreLoadRelatedData
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int TeamId { get; set; }
+    }
+}
