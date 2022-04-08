fc3f56a89700910d528cb8dcccef09fd52578b2f
mustafaiam <mustafaiam.github@gmail.com>
2022-04-08 16:28:39 +0300

    Implemented the Team class

---
+++ EFCoreLoadRelatedData/EFCoreLoadRelatedData/Team.cs
@@ -0,0 +1,13 @@
+﻿namespace EFCoreLoadRelatedData
+{
+    public class Team
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int ComicId { get; set; }
+
+        public List<SuperHero> SuperHeroes { get; set; } = new List<SuperHero>();
+    }
+}
