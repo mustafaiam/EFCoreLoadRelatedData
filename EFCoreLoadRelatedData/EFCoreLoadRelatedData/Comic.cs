46172e44ba496aa7d279ed1664e6da49b64c779b
mustafaiam <mustafaiam.github@gmail.com>
2022-04-08 16:29:46 +0300

    Implemented the Comic class

---
+++ EFCoreLoadRelatedData/EFCoreLoadRelatedData/Comic.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCoreLoadRelatedData
+{
+    public class Comic
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public List<Team> Teams { get; set; } = new List<Team>();
+    }
+}
